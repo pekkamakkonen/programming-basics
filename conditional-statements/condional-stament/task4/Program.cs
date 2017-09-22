ef7b9ebe5a368183574807d661dd3cfdbc550f0b
1701691-Saimia <pek.makkonen@gmail.com>
2017-09-22 15:28:47 +0300

    Task4

---
+++ conditional-statements/condional-stament/task4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna numero: ");
+            String userInput;
+            userInput = Console.ReadLine();
+            int luku1;
+            luku1 = int.Parse(userInput);
+
+            Console.WriteLine("Anna numero: ");
+            String userInput2;
+            userInput2 = Console.ReadLine();
+            int luku2;
+            luku2 = int.Parse(userInput2);
+
+            Console.WriteLine("Anna numero: ");
+            String userInput3;
+            userInput3 = Console.ReadLine();
+            int luku3;
+            luku3 = int.Parse(userInput3);
+
+            int[] values = { luku1, luku2, luku3 };
+
+            Array.Sort(values);
+            foreach (int value in values)
+            {
+                Console.Write(value);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}

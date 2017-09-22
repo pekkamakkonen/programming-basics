878f64b7a9a0411dcfe1cbf9899ef94da6fd3dbd
1701691-Saimia <pek.makkonen@gmail.com>
2017-09-22 14:42:17 +0300

    Initial project

---
+++ conditional-statements/condional-stament/task3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // define instructions
+
+            Console.WriteLine("Anna numero: ");
+
+            // define variables
+
+            String userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            bool isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            // program logic
+
+            if (isNumber == true)
+            {
+
+                if (evaluatedNumber > 0)
+                {
+                    Console.Write($"Numero {evaluatedNumber} on positiivinen ja se on");
+
+                    if (evaluatedNumber % 2 == 0)
+                    {
+                        Console.WriteLine(" parillinen.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" pariton.");
+                    }
+
+                }
+                else if (evaluatedNumber < 0)
+                {
+                    Console.Write($"Numero {evaluatedNumber} on negatiivinen ja se on");
+
+                    if (evaluatedNumber % 2 == 0)
+                    {
+                        Console.WriteLine(" parillinen.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" pariton.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"Numero {evaluatedNumber} on nolla ja se on");
+                    Console.WriteLine(" parillinen.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Syötit muuta kuin numeroita.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}

4db003088b71b0aae92e7f353af60464f794fdd7
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-09-27 18:45:00 +0300

    Loop task one works

---
+++ loop-statesments/task-1/task-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Syötä numero: ");
+            String userInput;
+            userInput = Console.ReadLine();
+            int n;
+            bool isNumber = int.TryParse(userInput, out n);
+
+            if (isNumber == true && n >= 0) {
+
+                int x = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    x = x * i; 
+                }
+                Console.WriteLine($"Vastaus: {x}");
+
+            }
+            else {
+                Console.WriteLine("Määrittelemätön");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

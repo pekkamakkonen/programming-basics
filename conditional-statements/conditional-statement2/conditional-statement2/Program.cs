0461e318f0d21b4d58328e615993a2c12b7224a6
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-10-04 21:28:02 +0300

    Kesken

---
+++ conditional-statements/conditional-statement2/conditional-statement2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace conditional_statement2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Syötä ikäsi: ");
+
+            String userInput;
+            userInput = Console.ReadLine();
+
+            int age;
+            bool isNumber = int.TryParse(userInput, out age);
+
+            if(isNumber == true)
+            {
+                double normalPrice = 16.00;
+                double discount;
+
+                if (age < 7)
+                {
+                    discount = 1.00;
+                }
+                else if ((age >= 7 && age <= 15) || (age >= 65)) {
+                    discount = 0.50;
+                }
+                else if (age)
+                {
+                    discount = 0.50;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Virheellinen ikä!");
+            }
+            Console.ReadKey();
+        }
+    }
+}

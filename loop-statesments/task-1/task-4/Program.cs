ae6ec644749be74f5e7a6c085dd8504508287066
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-10-08 16:31:20 +0300

    Tasks 4/5 done

---
+++ loop-statesments/task-1/task-4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Syötä numero: ");
+            String userInput;
+            userInput = Console.ReadLine();
+            int n;
+            bool isNumber = int.TryParse(userInput, out n);
+
+            if (isNumber == true && n >= 1)
+            {
+
+                int x = 0;
+
+                for (int i = 1; i <= n; i++)
+                    {
+                        x = x + i;
+                    }
+                Console.WriteLine($"Vastaus: {x}");
+            }
+
+            else if (isNumber == true && n <= 1)
+            {
+                int x = 0;
+
+                for (int i = -1; i >= n; i--)
+                {
+                        x = x + i;
+                }
+                Console.WriteLine($"Vastaus: {x}");
+            }
+            else
+            {
+                Console.WriteLine("Määrittelemätön");
+            }
+            Console.ReadKey();
+        }
+    }
+}

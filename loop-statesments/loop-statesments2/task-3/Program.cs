4f34f1d00b2af2462a9b6b7f35e6641be1f29946
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-10-11 19:12:57 +0300

    Task 3 kesken

---
+++ loop-statesments/loop-statesments2/task-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int i = 1;
+
+            while (i <= 20) {
+                i++;
+
+                int satunnainen = rnd.Next(50);
+                Console.WriteLine(satunnainen);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}

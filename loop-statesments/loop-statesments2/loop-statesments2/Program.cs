a55aa08d92c3755efbc55bbb4ae36a6d53ef3e9b
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-10-11 17:53:47 +0300

    Loop-Statesments kertotaulu tehty

---
+++ loop-statesments/loop-statesments2/loop-statesments2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace loop_statesments2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luku | Neliojuuri");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($" {i} | { Math.Round(Math.Sqrt(i), 2) }");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ loop-statesments/loop-statesments2/task-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vastaus: ");
+
+            for (int y = 1; y <= 9; y++)
+            {
+                for (int x = 1; x <= 9; x++)
+                {
+                    int tulos;
+                    tulos = x * y;
+                    Console.WriteLine($"{x} x {y} = {tulos}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

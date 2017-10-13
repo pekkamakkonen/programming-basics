3269a151ee3240d4ae45b6cbc98ed9052c32fbf7
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-10-13 20:42:08 +0300

    Loop 4.1.3 ready

--- loop-statesments/loop-statesments2/task-3/Program.cs
+++ loop-statesments/loop-statesments2/task-3/Program.cs
@@ -8,16 +8,41 @@ namespace task_3
         {
             Random rnd = new Random();
 
-            int i = 1;
-
-            while (i <= 20) {
-                i++;
-
-                int satunnainen = rnd.Next(50);
-                Console.WriteLine(satunnainen);
+            for (int laskuri = 0; laskuri < 4; laskuri++)
+            {
+                Console.WriteLine("Rivi {0}:{1}", laskuri + 1, luvutListaan(rnd));
             }
             Console.ReadKey();
+        }
+            static string luvutListaan(Random rnd)
+            {
+                int arpa = 0;
+                string msg = "";
+                for (int luku = 0; luku < 5; luku++)
+                {
+                    arpa = rnd.Next(51);
 
+                    if (luku < 4)
+                    {
+                        msg += string.Format(" {0}, ", muotoileLuku(arpa));
+                    }
+                    else
+                    {
+                    msg += string.Format(" {0} ", muotoileLuku(arpa));
+                    }
+                }
+                return msg;
+            }
+
+            static string muotoileLuku(int x)
+            {
+                string erotin = "";
+
+                if (x < 10)
+                {
+                    erotin = " ";
+                }
+                return ($" { erotin } { x.ToString() }");
+            }
         }
     }
-}

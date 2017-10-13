7964d9ecf0bcbbba3abfab217e83f039dbe9db33
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-10-14 00:50:42 +0300

    Loop 4.1.5 ready

---
+++ loop-statesments/loop-statesments2/task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            double luku;
+            char merkki;
+
+            for (int i = 1; i <= 13; i++)
+            {
+                luku = rnd.NextDouble();
+
+                if (luku <= 0.4)
+                {
+                    merkki = '1';
+                }
+                else if(luku <= 0.6)
+                {
+                    merkki = 'x';
+                }
+                else
+                {
+                    merkki = '2';
+                }
+                Console.WriteLine($" { i }. { merkki } ");
+            }
+            Console.ReadKey();
+        }
+    }
+}

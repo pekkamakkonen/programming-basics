6f6d47038c2a9a558f4945e42ba0c2a530cf9ae6
1701691-Saimia <pek.makkonen@gmail.com>
2017-10-24 09:45:53 +0300

    Function task 4 ready!

--- functions/functions-1/task-3/Program.cs
+++ functions/functions-1/task-3/Program.cs
@@ -10,10 +10,6 @@ namespace task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Syota luku valilta 1-20: ");
-            string userInput = Console.ReadLine();
-            int number = int.Parse(userInput);
-
             int lowerBound = 1;
             int upperBound = 20;
             Console.WriteLine(NumberFromRange(lowerBound, upperBound));
@@ -22,20 +18,28 @@ namespace task_3
 
         static int NumberFromRange (int lowerBound, int upperBound)
         {
-
-            bool numberIsOkay = false;
+            bool numberIsOkay;
+            int number;
+            number = 0;
 
             do
             {
+                Console.WriteLine("Syota luku valilta 1-20: ");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
 
                 if (number >= lowerBound && number <= upperBound)
                 {
                     numberIsOkay = true;
                 }
+                else
+                {
+                    numberIsOkay = false;
+                }
 
             } while (numberIsOkay == false);
 
-            return number;            
+            return number;
         }
     }
 }

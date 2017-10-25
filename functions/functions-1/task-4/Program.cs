b5cc1e05b22dc2de0b5a9634c0fdb7fc1f857115
1701691-Saimia <pek.makkonen@gmail.com>
2017-10-25 09:41:45 +0300

    Task 4 works, but without used function. (Work In Progress)

---
+++ functions/functions-1/task-4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int j;
+            string userInput = "";
+            int biggestNumber = 0;
+            string lukujono = "";
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Syota luku {i}.");
+                userInput = Console.ReadLine();
+                j = int.Parse(userInput);
+
+                Console.WriteLine(j);
+
+                if (j > biggestNumber)
+                {
+                    biggestNumber = j;
+                }
+
+                lukujono = lukujono + " " + j;
+               
+            }
+            Console.WriteLine($"Syötit luvut { lukujono }. \n Suurin luku oli { biggestNumber }. ");
+            Console.ReadKey();
+
+        }
+    }
+}

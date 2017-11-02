b52526dbb321486fea66b3eaa57ac6c7302529fa
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-11-03 00:56:48 +0200

    Array 1 done

---
+++ arrays/arrays-1/arrays-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace arrays_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] iT = new int[100];
+            double sum = 0;
+            double average;
+
+            Console.WriteLine("Arvotut luvut ovat: ");
+
+            for (int i = 0; i < iT.Length; i++)
+            {
+                iT[i] = rnd.Next(0, 51);
+                Console.WriteLine($"{ i + 1 }. { iT[i] }");
+                sum = (double)sum + iT[i];
+            }
+
+            // Lasketaan lukujen keskiarvo:
+
+            average = sum / iT.Length;
+
+            Console.WriteLine($"Lukujen keskiarvo on { average }.");
+            Console.ReadKey();
+        }
+    }
+}

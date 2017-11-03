3a0d170802c85bad748515dd3bb818d338cb330f
1701691-Saimia <pek.makkonen@gmail.com>
2017-11-03 14:49:29 +0200

    Lotto Work In Progress

---
+++ arrays/lotto/lotto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[7];
+
+            for (int i = 0; i <= 6; i++)
+            {
+                numbers[i] = rnd.Next(1, 41);
+            }
+            Console.WriteLine("Illan lottonumerot ovat: ");
+
+            Array.Sort(numbers);
+
+            foreach (int number in numbers)
+            {
+
+                Console.Write(number);
+                Console.Write(' ');
+
+            }
+            Console.ReadKey();
+        }
+    }
+}

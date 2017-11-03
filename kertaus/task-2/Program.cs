801c09c263896765b1a8bbabfb58f6526b18358a
1701691-Saimia <pek.makkonen@gmail.com>
2017-11-03 12:54:24 +0200

    kertaustehtävät done!

---
+++ kertaus/task-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä sana tai lause: ");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                Console.WriteLine($" { i }. { userInput }");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kertaus/task-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+            string userInput;
+
+            do
+            {
+                Console.WriteLine("Syötä luku: ");
+                userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                sum = sum + number;
+            } while (number != -1);
+
+            Console.WriteLine($"Lukujen summa on { sum }.");
+            Console.ReadKey();
+        }
+    }
+}

c9a1cb56c2537506d83323766e31b0316c5ef2ba
1701691-Saimia <pek.makkonen@gmail.com>
2017-11-10 10:36:18 +0200

    Array 3.1 works, but without function.

---
+++ arrays/taulukko3/taulukko3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taulukko3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] array = new int[10];
+
+            for (int i = 0; i <= 9; i++)
+            {
+                array[i] = rnd.Next(0, 20);
+
+                if (array[i] < 10)
+                {
+                    Console.WriteLine($"0{ array[i] }");
+                }
+                else
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

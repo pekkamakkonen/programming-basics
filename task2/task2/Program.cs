5a92affaa7659e33c5b1aebc160845b0d4092b75
1701691-Saimia <pek.makkonen@gmail.com>
2017-11-16 13:35:06 +0200

    Kertaus 16.11.

---
+++ task2/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(1, 101);
+            int number;
+            int arvaus = 0;
+            Console.WriteLine("Minäpä tiedän luvun väliltä 1-100, jota sinä et tiedä!");
+
+            do
+            {
+                Console.WriteLine("Arvaa luku:");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                arvaus++;
+            } while (number != x);
+
+            if(number == x)
+            {
+                Console.WriteLine($"Oikein! Arvauksia yhteensä: {arvaus}");
+            }
+            Console.ReadKey();
+        }
+    }
+}

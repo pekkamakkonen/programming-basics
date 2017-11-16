8263cef8ab1711254cc070987e4ab3fa66c0d6a2
1701691-Saimia <pek.makkonen@gmail.com>
2017-11-16 13:32:23 +0200

    Kertaus 16.11.

---
+++ kertaus3/kertaus3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kertaus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+
+            do
+            {
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                int[] numbers = 
+
+            } while (number != number);
+        }
+    }
+}

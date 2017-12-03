ba2dc0b5e6ff9cef395ec4203e51b7abac445705
Pekka Makkonen <pekka.makkonen@student.saimia.fi>
2017-12-03 17:42:23 +0200

    String handlings 4/4 done!

---
+++ string-handling/string-handling1/task-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syota sana. Ohjelma tarkistaa onko sana palintromi.");
+            string userInput = Console.ReadLine();
+            string syote = userInput.ToUpper();
+            string reversed = "";
+
+            foreach (char x in syote)
+            {
+                reversed = x + reversed;
+            }
+
+            string message = "";
+
+            if (syote == reversed)
+            {
+                message = ($"Sana { syote } on palintromi");
+            }
+            else
+            {
+                message = ($"Sana { syote } ei ole palintromi");
+            }
+            Console.WriteLine($"{ message }");
+            Console.ReadKey();
+        }
+    }
+}

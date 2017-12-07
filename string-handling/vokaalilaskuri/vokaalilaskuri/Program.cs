a70824079ffff6237d392a9f39f026d85dc2b84d
1701691-Saimia <pek.makkonen@gmail.com>
2017-12-07 12:52:59 +0200

    vokaalilaskuri done

---
+++ string-handling/vokaalilaskuri/vokaalilaskuri/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace vokaalilaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Syötä sana tai lause.");
+            string userInput;
+            userInput = Console.ReadLine();
+            string syote = userInput.ToUpper();
+            int vokaali = 0;
+
+            foreach(char x in syote)
+            {
+                if(x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U' || x == 'Y' || x == 'Ä' || x == 'Ö')
+                {
+                    vokaali = vokaali + 1;
+                }
+            }
+            Console.WriteLine($"Sanassa { userInput } on { vokaali } vokaalia");
+            Console.ReadKey();
+
+        
+        }
+    }
+}
